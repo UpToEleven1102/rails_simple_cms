9ec08c42d0adce241a9a97432ed9a507507e2540
HuyenV <huyen.vu@nextgencode.io>
2020-11-29 19:57:42 -0600

    added simple about us and contact us

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -8,6 +8,24 @@ class DemoController < ApplicationController
     #render('index')
   end
 
+  def about
+    render('about_us')
+  end
+
+  def contact
+    country = params['country']
+    @phone_number
+    if country == 'us' || country == 'ca'
+      @phone_number = '(800) 555 - 6789'
+    elsif country == 'uk'
+      @phone_number = "(020) 7946 1234"
+    else
+      @phone_number = "+1 (987) 654-3210"
+    end
+
+    render('contact_us')
+  end
+
   def student
     redirect_to(:controller => 'students', :action => 'index')
   end
